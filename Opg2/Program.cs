9e20fabb8fa4c2f931a5c8c718a301a1b90fddb9
Nicklas Pedersen <zbcniped37@zbc.dk>
2018-04-24 11:43:34 +0200

    Add project files.

---
+++ Opg2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, 1000).Select(x => x == 2 || x == 3 || (x % 2 != 0 && x != 1 && !Enumerable.Range(3, x / 2).Select(y => x % 2 == 0 || x % y != 0 ? null : "false").Contains("false")) ? x + "\n" : "").ToList().ForEach(i => Console.Write(i));
+            Enumerable.Range(1, 1000).Select(x => x == 2 || x == 3 || (x%2!=0 && x != 1 && !Enumerable.Range(3, x/2).Select(y => x%2==0||x % y != 0 ? null : "false").Contains("false")) ? x + "\n" : "").ToList().ForEach(i => Console.Write(i));
+            Console.ReadLine();
+        }
+    }
+}
